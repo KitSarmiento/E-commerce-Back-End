7217efde5a5f4daebd714deef9b577dfd1f66eec
KitSarmiento <ksarmiento020@gmail.com>
2023-10-09 18:36:36 -0400

    found an error in line 6 & 10

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 // see the activity 13-11 RESTful - Routes as a reference
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categories = async Category.findAll({
+    const categories = await Category.findAll({
       include: [{ model: Product }],
     });
     res.status(200).json(categories);
@@ -16,20 +16,20 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
