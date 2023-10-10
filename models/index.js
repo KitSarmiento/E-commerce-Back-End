83648fdd7e502e67dc670a40d7436f58bccf3f49
KitSarmiento <ksarmiento020@gmail.com>
2023-10-09 20:43:44 -0400

    Added a sequelize model associated for the product, category, tag and product tag.

--- models/index.js
+++ models/index.js
@@ -1,16 +1,34 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Class acitivity 13-24 was used as a reference for the codes below.
 
+// Products belongs To Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "product_tags",
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "product_tags",
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
