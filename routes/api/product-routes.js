dbf58d4bc93e2793b3c842b993b4fa154329a852
KitSarmiento <ksarmiento020@gmail.com>
2023-10-10 23:15:04 -0400

    A Delete route to delete a product by its id.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,6 +113,20 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      if (!product) {
+        res.status(404).json({ message: "No product found with this id!" });
+        return;
+      }
+      res.json({ message: "Product deleted successfully!" });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
-
 module.exports = router;
