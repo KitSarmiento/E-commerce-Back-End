7c48a9eee13b5b5dbf6fade1b75dc58fa3364fcb
KitSarmiento <ksarmiento020@gmail.com>
2023-10-10 22:15:16 -0400

    Added a Put and Delete routes for the user to be able to update amd delete the tag.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,12 +42,35 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = Tag.findByPk(req.params.id);
+    if (!updatedTag) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+    await updatedTag.update(req.body);
+    res.json(updatedTag);
+    res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = Tag.findByPk(req.params.id);
+    if (!deletedTag) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+    deletedTag.destroy();
+    res.json(deletedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
